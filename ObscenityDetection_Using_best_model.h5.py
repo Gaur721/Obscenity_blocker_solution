11aef5c077c1d1e0d699b0e65fd4c6b887594086
Gaurav Sonawane <104220519+Gaur721@users.noreply.github.com>
2023-08-02 17:22:46 +0530

    "modelnew_obs.h5" is a model for obscenity blocker with 94% accuracy

---
+++ ObscenityDetection_Using_best_model.h5.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from keras.models import load_model
+from collections import deque
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import cv2
+
+
+# In[ ]:
+
+
+#WORKING PROJECT
+import numpy as np
+import argparse
+import pickle
+import cv2
+from google.colab.patches import cv2_imshow
+import os
+import time
+from keras.models import load_model
+from collections import deque
+
+def print_results(video, limit=None):
+        #fig=plt.figure(figsize=(16, 30))
+        if not os.path.exists('output'):
+            os.mkdir('output')
+
+        print("Loading model ...")
+        model = load_model("C:\Users\Gaurav\OneDrive\Documents\Image_Video obscenity\best_model.h5")
+        Q = deque(maxlen=128)
+        vs = cv2.VideoCapture(video)
+        writer = None
+        (W, H) = (None, None)
+        count = 0
+        while True:
+            # read the next frame from the file
+            (grabbed, frame) = vs.read()
+
+            # if the frame was not grabbed, then we have reached the end
+            # of the stream
+            if not grabbed:
+                break
+
+            # if the frame dimensions are empty, grab them
+            if W is None or H is None:
+                (H, W) = frame.shape[:2]
+
+            # clone the output frame, then convert it from BGR to RGB
+            # ordering, resize the frame to a fixed 128x128, and then
+            # perform mean subtraction
+
+
+            output = frame.copy()
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.resize(frame, (128, 128)).astype("float32")
+            frame = frame.reshape(128, 128, 3) / 255
+
+            # make predictions on the frame and then update the predictions
+            # queue
+            preds = model.predict(np.expand_dims(frame, axis=0))[0]
+#             print("preds",preds)
+            Q.append(preds)
+
+            # perform prediction averaging over the current history of
+            # previous predictions
+            results = np.array(Q).mean(axis=0)
+            i = (preds > 0.50)[0]
+            label = i
+
+            text_color = (0, 255, 0) # default : green
+
+            if label: # Violence prob
+                text_color = (0, 0, 255) # red
+
+            else:
+                text_color = (0, 255, 0)
+
+            text = "Obscene: {}".format(label)
+            FONT = cv2.FONT_HERSHEY_SIMPLEX
+
+            cv2.putText(output, text, (35, 50), FONT,1.25, text_color, 3)
+
+            # check if the video writer is None
+            if writer is None:
+                # initialize our video writer
+                fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+                writer = cv2.VideoWriter("output/v_output.avi", fourcc, 30,(W, H), True)
+
+            # write the output frame to disk
+            writer.write(output)
+
+            # show the output image
+            cv2_imshow(output)
+            key = cv2.waitKey(1) & 0xFF
+
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
+        # release the file pointersq
+        print("[INFO] cleaning up...")
+        writer.release()
+        vs.release()
+
+
+# In[ ]:
+
+
+obsce_path = "C:\Users\Gaurav\OneDrive\Documents\video dataset\Obscene_video\all television.mp4"
+nonobscene_path = "/content/drive/MyDrive/Non-obscene_videos /VID-20230418-WA0005.mp4"
+
+
+# In[ ]:
+
+
+print_results(obsce_path)
+
